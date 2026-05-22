7412797b04631f8e4033705966cd1045d534fa03
myeonghwi <automaster5013@gmail.com>
2026-05-22 14:59:46 +0900

    교재_122page, Mixins

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,11 @@
 #example/urls.py
 from django.urls import path, include
-from .views import HelloAPI, bookAPI, booksAPI
+from .views import  HelloAPI, bookAPI, booksAPI, BooksAPI, BookAPI
 
 urlpatterns = [
     path("hello/", HelloAPI),
     path("fbv/books/", booksAPI),
     path("fbv/book/<int:bid>/", bookAPI),
+    path("cbv/books/", BooksAPI.as_view()),
+    path("cbv/book/<int:bid>/", BookAPI.as_view()),
 ]
